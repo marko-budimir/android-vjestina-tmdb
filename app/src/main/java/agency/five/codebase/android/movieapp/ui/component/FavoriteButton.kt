4fa98221c772f4c2abcdd50f131d160732f85bd7
Marko Budimir <marko.budimir12@gmail.com>
2022-11-10 04:23:23 +0100

    Edit FavoriteButton.kt

--- app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/component/FavoriteButton.kt
@@ -1,41 +1,49 @@
 package agency.five.codebase.android.movieapp.ui.component
 
 import agency.five.codebase.android.movieapp.R
+import agency.five.codebase.android.movieapp.ui.theme.spacing
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.runtime.Composable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
-private var isFavorite = false
 
 @Composable
 fun FavoriteButton(
-    backgroundColor: Color
+    backgroundColor: Color,
+    isFavorite: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     Image(
         painter = painterResource(id = if (isFavorite) R.drawable.ic_favorite_filled else R.drawable.ic_favorite),
         contentDescription = null,
-        modifier = Modifier
+        modifier = modifier
             .clickable {
-                isFavorite = isFavorite.not()
+                onClick()
             }
-            .size(32.dp)
+            .size(dimensionResource(id = R.dimen.favorite_button_size))
             .background(backgroundColor, CircleShape)
-            .padding(8.dp)
+            .padding(MaterialTheme.spacing.small)
     )
 }
 
 @Preview
 @Composable
 private fun FavoriteButtonPreview() {
-    FavoriteButton(colorResource(id = R.color.favorite_button_background))
+    var isFavorite by remember { mutableStateOf(true) }
+    FavoriteButton(
+        backgroundColor = colorResource(id = R.color.favorite_button_background),
+        isFavorite = isFavorite,
+        onClick = { isFavorite = !isFavorite }
+    )
 }
