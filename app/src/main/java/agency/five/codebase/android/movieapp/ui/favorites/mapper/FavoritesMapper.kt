1abc1397333d6763b617bebd73e7c7455167280b
Marko Budimir <marko.budimir12@gmail.com>
2022-11-18 22:13:07 +0100

    Add FavoritesMapper and FavoritesMapperImpl

---
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/favorites/mapper/FavoritesMapper.kt
@@ -0,0 +1,9 @@
+package agency.five.codebase.android.movieapp.ui.favorites.mapper
+
+import agency.five.codebase.android.movieapp.model.Movie
+import agency.five.codebase.android.movieapp.ui.favorites.FavoritesViewState
+
+interface FavoritesMapper {
+
+    fun toFavoritesViewState(favoritesMovies: List<Movie>): FavoritesViewState
+}
---
+++ app/src/main/java/agency/five/codebase/android/movieapp/ui/favorites/mapper/FavoritesMapperImpl.kt
@@ -0,0 +1,14 @@
+package agency.five.codebase.android.movieapp.ui.favorites.mapper
+
+import agency.five.codebase.android.movieapp.model.Movie
+import agency.five.codebase.android.movieapp.ui.component.MovieCardViewState
+import agency.five.codebase.android.movieapp.ui.favorites.FavoritesMovieViewState
+import agency.five.codebase.android.movieapp.ui.favorites.FavoritesViewState
+
+class FavoritesMapperImpl : FavoritesMapper {
+
+    override fun toFavoritesViewState(favoritesMovies: List<Movie>): FavoritesViewState =
+        FavoritesViewState(favoritesMovies.map {
+            FavoritesMovieViewState(it.id, MovieCardViewState(it.imageUrl, it.isFavorite))
+        })
+}
