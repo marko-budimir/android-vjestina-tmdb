dc218cf711e8baef5b7d68f9bbdf27d5b2377d0b
Marko Budimir <marko.budimir12@gmail.com>
2022-12-01 02:03:06 +0100

    Add DataModule

---
+++ app/src/main/java/agency/five/codebase/android/movieapp/data/di/DataModule.kt
@@ -0,0 +1,14 @@
+package agency.five.codebase.android.movieapp.data.di
+
+import agency.five.codebase.android.movieapp.data.repository.FakeMovieRepository
+import agency.five.codebase.android.movieapp.data.repository.MovieRepository
+import kotlinx.coroutines.Dispatchers
+
+
+import org.koin.dsl.module
+
+val dataModule = module {
+    single<MovieRepository> {
+        FakeMovieRepository(ioDispatcher = Dispatchers.IO)
+    }
+}
